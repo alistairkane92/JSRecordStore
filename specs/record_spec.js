fb9838cf683f019f956fffc22fb5184726e48d2c
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-01 14:51:53 +0000

    Sets up Record constructor and renames some pending tests

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -3,11 +3,15 @@ var Record = require('../record');
 
 describe("Record", function(){
     beforeEach(function(){
+        record1 = new Record("Black Album", "Metallica", "Metal", 5);
     })
 
-    it("should have a name")
-    it("should have a title")
+    it("should have a title", function(){
+        assert.strictEqual(record1.title, "Black Album");
+    })
+
+    it("should have an artist")
     it("should have a genre")
     it("should have a price")
-    
+
 })
