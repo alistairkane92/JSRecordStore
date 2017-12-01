df7dc9eff9053a43a3e835bbac328b0d7293a077
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-01 14:44:10 +0000

    Adds tests for Record

---
+++ specs/record_spec.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var Record = require('../record');
+
+describe("Record", function(){
+    beforeEach(function(){
+    })
+
+    it("should have a name")
+    it("should have a title")
+    it("should have a genre")
+    it("should have a price")
+    
+})
