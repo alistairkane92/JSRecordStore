5c79e7c23fceab85aa1529bb4245b051312149ff
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-01 15:02:36 +0000

    sets up spec file and all pending tests

---
+++ specs/record_store_spec.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var Record = require('../record');
+var RecordStore = require('../record_store');
+
+describe("Record", function(){
+    beforeEach(function(){
+    })
+
+    it("should have a name")
+    it("should have a city")
+    it("should have a balance")
+    it("should have an inventory")
+    it("should be able to add records to inventory")
+    it("should be able to remove a record from inventory")
+    it("should be able to display all records of inventory")
+    it("should be able to sell a record")
+    it("should be able to calculate the total value of inventory")
+    it("should be able to return finances + total value of inventory")
+    it("should be able to return all records by a specific genre")
+
+})
