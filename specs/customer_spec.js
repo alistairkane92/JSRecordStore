0a031f3dc358a4d663276f721e96a25541b67c06
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-03 11:52:06 +0000

    Adds all pending tests for customer

---
+++ specs/customer_spec.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var Record = require('../record');
+var Customer = require('../customer');
+var Inventory = require('../inventory');
+
+describe("Customer", function(){
+    beforeEach(function(){
+
+    })
+
+    it("should have a name")
+    it("should have funds")
+    it("should be able to buy a record")
+    it("should be able to sell a record")
+    it("should not be able to buy if doesn't have enough funds")
+    it("should be able to view total value of inventory")
+    it("should be able to calculate total value of inventory")
+    it("should be able to calculate total by genre of inventory")
+    it("should be able to view the most valuable record")
+    it("should be able to sort records by value")
+    it("should be able to compare value of their collection with value of another")
+})
