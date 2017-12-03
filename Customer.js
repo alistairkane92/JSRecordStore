e22ab72d29bf646cfbfb0c0a851b5d27a8446da9
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-03 16:37:08 +0000

    saves files

---
+++ Customer.js
@@ -0,0 +1,47 @@
+var Customer = function(name, inventory, funds){
+    this.name = name;
+    this.inventory = inventory;
+    this.funds = funds;
+}
+
+Customer.prototype = {
+    getInventory: function(){
+        return this.inventory.stock;
+    },
+    buy: function(record){
+        if (this.funds > record.price){
+            this.inventory.add(record);
+            this.funds -= record.price;
+        }
+    },
+    sell: function(record){
+        this.inventory.remove(record);
+        this.funds += record.price;
+    },
+    calculateTotal: function(){
+        return this.inventory.calculateTotal();
+    },
+    getByGenre: function(genre){
+        return this.inventory.getByGenre(genre);
+    },
+    getTotalByGenre: function(genre){
+        return this.inventory.getTotalByGenre(genre);
+    },
+    getMostValuable: function(){
+        return this.inventory.getMostValuable();
+    },
+    sortByValue: function(){
+        return this.inventory.sortByValue();
+    },
+    compareTotalValue: function(customer){
+        return this.name + ": " + this.calculateTotal() + ", "
+        + customer.name + ": " + customer.calculateTotal();
+    },
+    isValueGreater: function(customer){
+        if (this.calculateTotal() > customer.calculateTotal()){
+            return true;
+        } return false;
+    }
+}
+
+module.exports = Customer;
