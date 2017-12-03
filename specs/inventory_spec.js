f2fed9b451f325fab95d6d4981ecb75e33f16945
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-03 10:53:41 +0000

    Sets up pending tests taken frm recordstore_spec

---
+++ specs/inventory_spec.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+dsecribe("Inventory", function(){
+    beforeEach(function(){
+
+    });
+
+    it("should be able to add records to inventory")
+    it("should be able to remove a record from inventory")
+    it("should be able to display all records in inventory")
+    it("should be able to calculate the total value of inventory")
+    it("should be able to return all records by a specific genre")
+})
